429aa0dca6b37c470c1d32bfa1d5709233e32c3d
Hafiz Adeel <hafizadeel493@gmail.com>
2025-10-21 14:46:06 +0500

    Added generic spinner

--- src/app/user/dashboard/page.tsx
+++ src/app/user/dashboard/page.tsx
@@ -1,17 +1,18 @@
-import { Button } from "@/components/ui/button";
+import { getCurrentUser } from "@/actions/auth-actions";
 import RecentTickets from "../components/recent-tickets";
 import Stats from "../components/stats";
-import Link from "next/link";
 
-const UserDashboard = () => {
+const UserDashboard = async () => {
+  const { data: user } = await getCurrentUser();
   return (
     <main className="p-10 max-w-6xl mx-auto">
       <div className="space-y-10">
         <div className="flex justify-between items-center">
-          <h1 className="text-2xl font-bold">Welcome back, Adeel 👋</h1>
-          <Button>
-            <Link href="/user/tickets">New Ticket</Link>
-          </Button>
+          <h1 className="text-2xl font-semibold">
+            Welcome back,{" "}
+            <span className="text-blue-700">{user?.name || user?.email} </span>{" "}
+            👋
+          </h1>
         </div>
         <Stats />
         <RecentTickets />
